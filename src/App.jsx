00779313289e28be6406cfd52afa13e7d21339ba
Faruk Spahic <farukspahictz@gmail.com>
2023-04-16 12:59:26 +0200

    Deleted unused dependencies and converted app.js to app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
+
+export default App;

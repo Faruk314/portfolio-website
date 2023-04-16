16bfb47a0dd43057b261fedffaa9a78522f97313
Faruk Spahic <farukspahictz@gmail.com>
2023-04-16 17:27:41 +0200

    Deleted Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
-import Contact from "./components/Contact";
 
 function App() {
   return (

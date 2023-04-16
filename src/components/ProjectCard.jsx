f7fe7e72e6ad6d54bd187b2c6eae8f971c229086
Faruk Spahic <farukspahictz@gmail.com>
2023-04-16 13:08:57 +0200

    Added project card

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+import { RiGithubLine } from "react-icons/ri";
+
+const ProjectCard = ({ title, desc, technologies }) => {
+  return (
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+      <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+        <img
+          className="w-full h-[20rem] object-cover object-center"
+          src="https://dummyimage.com/720x400/cccccc/ffffff"
+          alt="Project 2"
+        />
+        <div className="p-4">
+          <h3 className="font-bold text-indigo-600 text-lg mb-2">{title}</h3>
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+
+          <div className="flex space-x-2 mt-3">
+            <button
+              href="https://github.com"
+              className="bg-gray-900 text-white px-4 rounded-md font-medium hover:bg-gray-800"
+            >
+              Live Demo
+            </button>
+
+            <button
+              href="https://github.com"
+              className="bg-gray-900 text-white px-4 py-2 rounded-md font-medium hover:bg-gray-800 flex items-center space-x-1"
+            >
+              <RiGithubLine />
+              <span>Git Hub</span>
+            </button>
+          </div>
+        </div>
+        <div className="bg-gray-100 p-4">
+          <span className="text-gray-500 text-sm">Technologies:</span>
+          <div className="h-[5rem]">
+            <ul className="flex flex-wrap text-gray-700">
+              {technologies?.map((item, index) => (
+                <li
+                  key={index}
+                  className="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 w-max h-max mt-2 mr-2"
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;

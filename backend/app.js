10b055f20027afc0e5a7e0165bb1e97d4b94db3a
Faruk Spahic <farukspahictz@gmail.com>
2023-07-02 23:08:16 +0200

    Add dockerfile for backend

--- backend/app.js
+++ backend/app.js
@@ -10,11 +10,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-  })
-);
+app.use(cors());
 
 app.use("/api/contact", contactRoutes);
 
